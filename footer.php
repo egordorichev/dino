779ae15f97ed95215d1b140685aed8f4c5ebd34e
Egor Dorichev <egordorichev@gmail.com>
2020-09-28 13:02:20 +0300

    Added highlight js

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 		</div>
 	</div>
 	<?php wp_footer(); ?>
+	<script>
+		document.addEventListener('DOMContentLoaded', (event) => {
+			document.querySelectorAll('pre code').forEach((block) => {
+				hljs.highlightBlock(block);
+			});
+		});
+	</script>
 </body>
 </html>
